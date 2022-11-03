63c4e42a30f6e5657069c51f87ce5034b6e7a02f
jerrifemidaniel <jerrifemidaniel@gmail.com>
2022-11-03 16:30:07 +0100

    (My week 2)

---
+++ week3/practice_1/target/debug/deps/practice_1.d
@@ -0,0 +1,5 @@
+C:\Users\TUF GAMING A17\Documents\j_danielCSC101\week3\practice_1\target\debug\deps\practice_1.exe: src\main.rs
+
+C:\Users\TUF GAMING A17\Documents\j_danielCSC101\week3\practice_1\target\debug\deps\practice_1.d: src\main.rs
+
+src\main.rs:
---
+++ week3/practice_1/target/debug/practice_1.d
@@ -0,0 +1,1 @@
+C:\Users\TUF\ GAMING\ A17\Documents\j_danielCSC101\week3\practice_1\target\debug\practice_1.exe: C:\Users\TUF\ GAMING\ A17\Documents\j_danielCSC101\week3\practice_1\src\main.rs
